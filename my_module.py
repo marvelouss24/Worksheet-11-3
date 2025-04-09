d87840a72175746e78d7a3cdf4000042c1f13e62
Marvelous Shabi <MarvelousShabi@gmail.com>
2025-04-09 10:06:57 -0400

    Added divide to my_module and a test for it in test_my_module

--- my_module.py
+++ my_module.py
@@ -28,3 +28,6 @@ class Solution(object):
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+
+def divide(a, b):
+    return a / b
--- test_my_module.py
+++ test_my_module.py
@@ -1,7 +1,12 @@
 from my_module import Solution
-
+from my_module import divide
 
 def test_twoSum():
     solution = Solution()
     result = solution.twoSum([1, 3, 5], 8)
     assert result == [1, 2]
+
+def test_divide():
+    result = divide(10, 2)
+    assert result == 5
+    
